afddad8792f6283d2acab9e2ada7a00ff61ce292
Matheus Maciel <eumatheusmaciel@gmail.com>
2025-03-09 13:17:51 -0300

    Integration with  S3 AWS
    Signed-off-by: Matheus Maciel <eumatheusmaciel@gmail.com>

---
+++ src/main/java/com/matheusmaciel/createUrlShortener/UrlData.java
@@ -0,0 +1,14 @@
+package com.matheusmaciel.createUrlShortener;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class UrlData {
+    private String originalUrl;
+    private long expirationTime;
+}
